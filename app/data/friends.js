59164602c253a2d90dce2bfc31cb735a6f2fc2a0
dair68 <granthuang68@yahoo.com>
2018-12-06 02:06:39 -0800

    server can now determine best friend match

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,34 @@
 var friends = [
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
-        "scores": [
-            "5",
-            "1",
-            "4",
-            "4",
-            "5",
-            "1",
-            "2",
-            "5",
-            "4",
-            "1"
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Grant",
+        photo: "https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/31164091_1320072551470202_1231956169937911808_n.jpg?_nc_cat=107&_nc_ht=scontent-lax3-2.xx&oh=4344440e1b663136e68d406b9ac5881b&oe=5CA49AE6",
+        scores: [
+            4,
+            4,
+            3,
+            4,
+            3,
+            2,
+            2,
+            1,
+            4,
+            2
         ]
     }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,12 +8,41 @@ module.exports = function (app, path) {
         res.json(friends);
     });
 
-    //updates stored friend data
+    //updates stored friend data and finds best match
     app.post("/api/friends", function (req, res) {
         var friend = req.body;
+
+        //finding friend with lowest absolute score difference
+        var scores = friend.scores; 
+        var scoreDifferences = [];
+        var minDifference = 40;
+        var best = friends[0];
+
+        //calculating the score difference for everyone in friends list
+        for(var i=0; i<friends.length; i++) {
+            var difference = 0;
+
+            for(var j=0; j<scores.length; j++) {
+                difference += Math.abs(friends[i].scores[j] - scores[j]);
+            }
+
+            scoreDifferences.push(difference);
+            if(difference < minDifference) {
+                minDifference = difference;
+                best = friends[i];
+            }
+        }
+
+        console.log(scoreDifferences);
+        console.log(minDifference);
+        console.log(best);
+
+        //adding newest person to friend data
         friends.push(friend);
 
         //displaying friends
         console.log("friend added");
+
+        return best;
     });
 }
