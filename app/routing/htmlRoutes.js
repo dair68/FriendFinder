deabb94d10de60619e6ef25b07ca63cc6dd81826
dair68 <granthuang68@yahoo.com>
2018-12-05 23:38:43 -0800

    created api routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,12 +1,12 @@
 module.exports = function (app, path) {
     //console.log("setting up html routes");
 
-    //going to home page
+    //pulls up home page
     app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
-    //going to survey page
+    //pulls up survey page
     app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
--- server.js
+++ server.js
@@ -4,6 +4,21 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var friends = [
+    {
+        name: "Ryan",
+        description: "Met through the coding bootcamp. My boyfriend."
+    },
+    {
+        name: "Sam",
+        description: "One of my roommates. Met him through church."
+    },
+    {
+        name: "Neil",
+        description: "One of my roommates. Met through GME."
+    }
+];
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,6 +26,20 @@ app.use(express.json());
 //setting up routing
 var htmlRoutes = require("./app/routing/htmlRoutes");
 htmlRoutes(app, path);
+
+//displays JSON of all possible friends
+app.get("/api/friends", function (req, res) {
+    res.json(friends);
+});
+
+//updates stored friend data
+app.post("/api/friends", function(req, res) {
+    var friend = req.body;
+    friends.push(friend);
+
+    //displaying friends
+    console.log(friends);
+});
 
 //listener
 app.listen(PORT, function () {
